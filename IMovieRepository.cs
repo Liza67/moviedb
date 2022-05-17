13d93857dba99366471cf20b7bd8dceafb9aafd7
Liza67 <87017817+Liza67@users.noreply.github.com>
2022-05-17 20:46:43 +0530

    Add files via upload

---
+++ IMovieRepository.cs
@@ -0,0 +1,14 @@
+﻿using MovieApplication.Models;
+
+namespace MovieApplication.Repositories
+{
+    public interface IMovieRepository
+    {
+        Task<IEnumerable<Models.Movie>> GetAll();
+        Task<IEnumerable<Models.Movie>> Get(int id);
+        Task<IEnumerable<Models.Movie>> Create(Models.Movie movie);
+        Task<IEnumerable<Models.Movie>> Update(Models.Movie movie);
+        Task Delete(int id);
+        Task<IEnumerable<Movie>> Get();
+    }
+}
