5ce2a3c4bd6321ca0e21347993b3509e089143a5
Liza67 <87017817+Liza67@users.noreply.github.com>
2022-05-17 20:47:39 +0530

    Add files via upload

---
+++ MovieRepositoryBase.cs
@@ -0,0 +1,15 @@
+﻿using MovieApplication.Models;
+
+namespace MovieApplication.Repositories
+{
+    public class MovieRepositoryBase
+    {
+        private readonly object _context;
+
+        public async Task<IEnumerable<Movie>> Create(Movie movie)
+        {
+
+            return (IEnumerable<Movie>)movie;
+        }
+    }
+}
