0b76e48a0a61d2fe85aaebabe686311ea5bcded5
Liza67 <87017817+Liza67@users.noreply.github.com>
2022-05-17 20:44:05 +0530

    Add files via upload

---
+++ MoviesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieApplication.Models;
+using MovieApplication.Repositories;
+
+namespace MovieApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMovieRepository _movieRepository;
+
+        public MoviesController(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<Movie>> GetMovies()
+        {
+            return await _movieRepository.Get();
+        }
+
+
+        [HttpGet("{id}")]
+        public Task<ActionResult<Movie>> GetMovies(int id, ActionResult<Movie> enumerable)
+        {
+            return Task.FromResult(enumerable);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Movie>> GetMovies(Movie movie)
+        {
+            var newMovie = await _movieRepository.Create(movie);
+            return CreatedAtAction(nameof(GetMovies), newMovie);
+        }
+    }
+}
