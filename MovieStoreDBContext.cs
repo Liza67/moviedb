dd7164628f4cfd0ff66b30a266336a8c67afe2e5
Liza67 <87017817+Liza67@users.noreply.github.com>
2022-05-17 20:45:27 +0530

    Add files via upload

---
+++ MovieStoreDBContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+
+namespace MovieApplication.Models
+{
+    public partial class MovieStoreDBContext : DbContext
+    {
+
+
+        public MovieStoreDBContext(DbContextOptions<MovieStoreDBContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public virtual DbSet<Movie> Movies { get; set; }
+        public virtual DbSet<Actor> Actors { get; set; }
+        public virtual DbSet<Producer> Producers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server-localhost;Database-Newmdb;Trusted_Connection-True;");
+            }
+        }
+
+
+    }
+}
