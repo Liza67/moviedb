dd1f55d13304ee66d8501d407b9897144dc72d2e
Liza67 <87017817+Liza67@users.noreply.github.com>
2022-05-17 20:47:10 +0530

    Add files via upload

---
+++ MovieRepository.cs
@@ -0,0 +1,39 @@
+﻿using MovieApplication.Models;
+
+namespace MovieApplication.Repositories
+{
+    public class MovieRepository : MovieRepositoryBase, IMovieRepository
+    {
+        private MovieStoreDBContext _context;
+
+
+        public MovieRepository(MovieStoreDBContext context)
+        {
+            _context = context;
+        }
+
+        public Movie movieToDelete { get; private set; }
+
+        public async Task Delete(int id)
+        {
+            var bookToDelete = await _context.Movies.FindAsync(id);
+            _context.Movies.Remove(movieToDelete);
+            await _context.SaveChangesAsync();
+        }
+
+        public Task<IEnumerable<Movie>> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Movie>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Movie>> Update(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
